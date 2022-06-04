93f35b7a06af4b5611098e79cf0432cb16d1a275
sohila samir <sohila.samir1911@gmail.com>
2022-06-04 15:35:53 +0200

    fixing minor bugs in the timer & adding a vibration option to mobile user on finishing

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
+// selecting all set time input fields to loop over them
 const timerInputs = document.querySelectorAll('.timer-input');
 
-const startBtn = document.querySelector('.start');
-const pauseBtn = document.querySelector('.pause')
-const cancelBtn = document.querySelector('.cancel')
-
+// input fields
 let hoursInput = document.querySelector('#hours');
 let minuteInput = document.querySelector('#minutes');
 let secondsInput = document.querySelector('#seconds');
 
+// helper buttons
+const startBtn = document.querySelector('.start');
+const pauseBtn = document.querySelector('.pause')
+const cancelBtn = document.querySelector('.cancel')
+
+// columns used to show the current time remaining
 let countDownHours = document.querySelector('#countDown-hours')
 let countDownMinutes = document.querySelector('#countDown-minutes')
 let countDownSeconds = document.querySelector('#countDown-seconds')
 
+// timer & set time container
 let setTimerContainer = document.querySelector('#set-time-container')
 let timerContainer = document.querySelector('.timer-container')
 
+// used variable in counting down process
 let reqHours = 0
 let reqMinutes = 0
 let reqSeconds = 0
 
+// times in milliseconds used for the setInterval
 let hoursInMS = 0
 let minutesInMs = 0
 let secondsInMs = 0
 
+// total time requested in milliseconds
 let timeInMillieSeconds;
+
+// the setInterval fun
 let countDownInterval;
+
+// start button tap index count
+let startBtnIndexCount = 0
 
 const initTimerValues = () => {
     for (let i = 0; i < timerInputs.length; i++) {
@@ -33,12 +46,15 @@ const initTimerValues = () => {
                 if (e.target.id === 'hours') {
                     reqHours = e.target.value
                     hoursInMS = e.target.value * 60 * 60 * 1000
+                    console.log('requested Hours: ', reqHours);
                 } else if (e.target.id === 'minutes') {
                     reqMinutes = e.target.value
                     minutesInMs = e.target.value * 60 * 1000;
+                    console.log('requested Minutes: ', reqMinutes);
                 } else if (e.target.id === 'seconds') {
                     reqSeconds = e.target.value
                     secondsInMs = e.target.value * 1000;
+                    console.log('requested Seconds: ', reqSeconds);
                 }
                 timeInMillieSeconds = hoursInMS  + minutesInMs + secondsInMs
             }
@@ -69,21 +85,19 @@ const resetTimerVariables = () => {
     reqHours = hoursInput.value
     reqMinutes = minuteInput.value
     reqSeconds = secondsInput.value
-    timeInMillieSeconds = 0
+    timeInMillieSeconds = hoursInMS + minutesInMs + secondsInMs
+    startBtnIndexCount = 0
 }
 
 const countDown = () => {
-    if (timeInMillieSeconds) {
+    if ((hoursInput.value || minuteInput.value || secondsInput.value) && timeInMillieSeconds > 0) {
         timeInMillieSeconds-=1000
-        // changing color when time is less than or equal to
-
         if (hoursInput.value > 0) {
             reqHours <= 10 ? countDownHours.innerText = `0${reqHours}:` : countDownHours.innerText = `${reqHours}:`
         }
         if (reqHours > 0 && reqMinutes === 0 && reqSeconds === 0) {
             reqHours <= 10 ? countDownHours.innerText = `0${--reqHours}:` : countDownHours.innerText = `${--reqHours}:`
             reqMinutes = 60
-            minuteInput.value = 60
         }
 
         if (minuteInput.value > 0) {
@@ -92,16 +106,16 @@ const countDown = () => {
         if (reqMinutes > 0 && reqSeconds === 0) {
             reqMinutes <= 10 ? countDownMinutes.innerText = `0${--reqMinutes}:` : countDownMinutes.innerText = `${--reqMinutes}:`
             reqSeconds = 60
-            secondsInput.value = 60
         }
 
         if (secondsInput.value > 0) {
-            if (reqSeconds <= 10) {
-                countDownSeconds.innerText = `0${--reqSeconds}`
+            if (reqSeconds <= 5 && Number(reqMinutes) === 0 && Number(reqHours) === 0) {
                 // change animation and timer clock color theme
-                if (Number(reqMinutes) === 0 && Number(reqHours === 0)) {
-                    timerContainer.classList.add('change-theme-color')
-                }
+                countDownSeconds.innerText = `0${--reqSeconds}`
+                console.log('changing color theme fired!');
+                timerContainer.classList.add('change-theme-color')
+            } else if (reqSeconds <= 10) {
+                countDownSeconds.innerText = `0${--reqSeconds}`
             } else {
                 countDownSeconds.innerText = `${--reqSeconds}`
             }
@@ -112,11 +126,14 @@ const countDown = () => {
         timerContainer.classList.remove('change-theme-color')
         resetTimerVariables()
         clearInterval(countDownInterval)
+        navigator.vibrate([200,100,200])
+        console.log('TIME\'S UP');
     }
 }
 
 startBtn.addEventListener('click', (e) => {
-    if (timeInMillieSeconds) {
+    startBtnIndexCount++
+    if ((hoursInput.value || minuteInput.value || secondsInput.value) && timeInMillieSeconds > 0 && startBtnIndexCount === 1) {
         addAnimations()
         timerContainer.style.animation = 'up-down 1.5s ease-in-out 0s normal infinite forwards running'
         countDownInterval = setInterval(() => {
@@ -142,16 +159,10 @@ pauseBtn.addEventListener('click', (e) => {
 })
 
 const resetEverything = () => {
-    hoursInput.value = 0
-    minuteInput.value = 0
-    secondsInput.value = 0
-    reqHours = 0
-    reqMinutes = 0
-    reqSeconds = 0
+    resetTimerVariables()
     countDownHours.innerText = '00:'
     countDownMinutes.innerText = '00:'
     countDownSeconds.innerText = '00'
-    timeInMillieSeconds = 0
 }
 
 cancelBtn.addEventListener('click', (e) => {
@@ -161,3 +172,8 @@ cancelBtn.addEventListener('click', (e) => {
     timerContainer.classList.remove('change-theme-color')
     clearInterval(countDownInterval)
 })
+
+
+class AnimationManipulation {
+
+}
